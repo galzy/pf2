e9423e1e921df4ad2ce380cea2ceac2dca8015de
Gal Ziv Yacimovich <29704767+galzy@users.noreply.github.com>
2024-01-08 09:30:33 +0100

    comments

---
+++ src/helpers/properties.js
@@ -0,0 +1,17 @@
+exports.ALL_PROPERTIES= [
+     "aliases",
+    "attitude",
+    "bearer",
+    "date",
+    "groups",
+    "hq",
+    "in",
+    "near",
+    "players",
+    "race",
+    "status",
+    "type",
+    "when",
+    "where"
+
+];
